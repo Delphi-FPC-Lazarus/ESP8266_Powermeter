18c9ee4d131609ca79347e2c2b8ba5265c1ad035
Peter Lorenz <support@peter-ebe.de>
2022-12-15 23:29:15 +0100

    current development powermeter working for sinusoidal power consumption

--- ESPpowermeter.ino
+++ ESPpowermeter.ino
@@ -3,6 +3,7 @@
 
   This is an simple project for ESP8266 (NodeMCU 1.1 ESP-12E) Module. 
   Webserver providing a few Endpoints for analog meassurement from adc (TC-9520256 / TA12-200)
+  for sinusoidal power consumption 
   
   by Peter Lorenz
   support@peter-ebe.de
@@ -16,9 +17,8 @@
 #include "WLANZUGANGSDATEN.h"
 
 #define ANALOG_IN 0
-#define CALPOWER 358
-#define CALVALUE 288
-#define CALVALUEOFFSET 0
+#define CALCURRENTPOWER 358
+#define CALCURRENTVALUE 288
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
@@ -33,6 +33,129 @@ IPAddress primaryDNS(192, 168, 1, 1);   //optional
 IPAddress secondaryDNS(192, 168, 1, 1); //optional
 
 ESP8266WebServer server(80);
+
+// --------------------------------------------
+
+// Messfunktionen 
+// Notiz: delay() < 1ms compiliert, tut aber nichts                                              
+// analogRead() ist träge, 0,1ms auf dem esp8266
+// Wechselspannungsmessung bzw. nur positive Halbwelle
+// 50hz = 20ms -> 200 Samples je Sinus 
+// Um dieses Signal zu messen muss die Anzahl der Samples mindest ein Sinus sein, oder ein gerades vielfaches davon
+const int sampleCount = 600;
+// nach dem sortieren die höchsten n values wegschmeißen 
+const int skipValues = 5;
+// und über die n niedrigeren mitteln
+const int measureValues = 10;
+
+// Für die Messung von dynamischen Lasten muss über einen längeren Zeitraum gemittelt werden
+const int avgCount = 100;
+bool avgActive = false;
+int avgValue = 0;
+int values[sampleCount];
+// iterieren über array über samplecount oder (sizeof(values) / sizeof(values[0]))
+
+void exValue(int i, int j) {
+ int temp = values[i];
+ values[i] = values[j];
+ values[j] = temp;
+}
+void qSort(int loIdx, int highIdx) {
+ int i = loIdx;
+ int j = highIdx;
+ int pivot = values[loIdx + (highIdx - loIdx) / 2];
+ while (i <= j) {
+   while (values[i] < pivot) {
+     i++;
+   }
+   while (values[j] > pivot) {
+     j--;
+   }
+   if (i <= j) {
+     exValue(i, j);
+     i++;
+     j--;
+   }
+ }
+ if (loIdx < j)
+   qSort(loIdx, j);
+ if (i < highIdx)
+   qSort(i, highIdx);
+}
+void sortValues() {
+  Serial.println("sortValues()");
+  qSort(0, sampleCount - 1); 
+}
+
+// über n Sinus (obere Halbwelle) sampeln
+void sampleValues() {
+  Serial.println("sampleValues()");
+  // hintereinander weg samplen, ohne Unterbrechung
+  for (int i = 0; i < sampleCount - 1 ; i++) 
+  {
+    values[i] = analogRead(ANALOG_IN);
+  } 
+}
+
+// Messen peak bzw. high
+int getMeasurement() {
+  Serial.println("getMeasurement()");
+
+  sampleValues();
+
+  yield();
+  ESP.wdtFeed();
+
+  /* einfach den Max wert suchen
+  int measurementValue = 0; 
+  for (int i = 0; i < sampleCount - 1 ; i++) 
+  {
+    if (values[i] > measurementValue) {
+      measurementValue = values[i];
+    }
+  }
+  return measurementValue;
+  */
+
+  sortValues();
+  // die oberen n values wegschmeißen, ausreißer
+  // und über die darauf folgenden values mitteln
+  int measurementValue = 0; 
+  int measurementValueCount = 0;
+  for (int i = sampleCount-1-skipValues-measureValues; i < sampleCount-1-skipValues ; i++) 
+  {
+    measurementValue += values[i];
+    measurementValueCount += 1;
+  }
+  measurementValue = measurementValue / measurementValueCount;
+  return measurementValue;
+}
+
+// Debugging
+String getValuesDebug() {
+  Serial.println("getValuesDebug()");
+  String valuesStr = "";
+
+  sampleValues();
+  for (int i = 0; i < sampleCount - 1 ; i++) 
+  {
+    valuesStr += values[i];
+    valuesStr += ";";
+  }  
+  valuesStr += "\r\n";
+
+  sortValues();
+  for (int i = 0; i < sampleCount - 1 ; i++) 
+  {
+    valuesStr += values[i];
+    valuesStr += ";";
+  }  
+  valuesStr += "\r\n";
+
+  return valuesStr;
+}
+
+// --------------------------------------------
 
 void handleRoot() {
   Serial.println("handleRoot()");
@@ -52,58 +175,34 @@ void handleRoot() {
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
 }
 
-void handleSampleValues() {
-  Serial.println("handleSampleValues()");
+void handleCurrentSampleValues() {
+  Serial.println("handleCurrentSampleValues()");
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  int values[500];
-  for (int i = 0; i < ( sizeof(values) / sizeof(values[0]) ) - 1 ; i++) 
-  {
-    values[i] = analogRead(ANALOG_IN); // relativ träge, 0,1ms auf dem esp8266
-    //delay() < 1ms compiliert, tut aber nichts                                              
-  } 
-  String message = "";
-  for (int i = 0; i < ( sizeof(values) / sizeof(values[0]) ) - 1 ; i++) 
-  {
-    message += values[i];
-    message += ";";
-  }
+
+  String message = String(getValuesDebug());
+
   server.send(200, "text/plain", message);
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
 }
 
-int getMaxValue() {
+void handleCurrentMeasurement() {
+  Serial.println("handleCurrentMeasurement()");
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  int values[500];
-  for (int i = 0; i < ( sizeof(values) / sizeof(values[0]) ) - 1 ; i++) 
-  {
-    values[i] = analogRead(ANALOG_IN); // relativ träge, 0,1ms auf dem esp8266
-    //delay() < 1ms compiliert, tut aber nichts                                               
-  }
-  int maxValue = 0; 
-  for (int i = 0; i < ( sizeof(values) / sizeof(values[0]) ) - 1 ; i++) 
-  {
-    if (values[i] > maxValue) {
-      maxValue = values[i];
-    }
-  }
-  return maxValue;
-}
 
-void handleMaxValue() {
-  Serial.println("handleMaxValue()");
+  String message = String(getMeasurement());
 
-  String message = String(getMaxValue());
   server.send(200, "text/plain", message);
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
 }
 
-void handleMeasurePower() {
-  Serial.println("handleMeasurePower");
+void handleCurrentPower() {
+  Serial.println("handleCurrentPower");
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
 
-  int value = getMaxValue();
+  int value = getMeasurement();
   Serial.println("Value:");
   Serial.println(value);
-  float power = float(CALPOWER)/float(CALVALUE) * value-CALVALUEOFFSET;
+  float power = float(CALCURRENTPOWER)/float(CALCURRENTVALUE) * value;
   Serial.println("Power");
   Serial.println(power);
 
@@ -112,39 +211,91 @@ void handleMeasurePower() {
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
 }
 
-void handleNotFound() {
-  Serial.println("handleNotFound()");
+// --------------------------------------------
+
+void handleAvgDo() {
+  Serial.println("handleAvgDo");
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+
+  // nur das Flag setzen
+  avgActive = true;
+
+  String message = "OK";
+  server.send(200, "text/plain", message);
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH  
+}
+
+void doAvgMeasurement() {
+  Serial.println("doAvgMeasurement");
+
+  for (int i = 0; i < 3; i++) 
+  {
+    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    delay(100);
+    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH 
+    delay(100);
+  } 
+
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+
+  avgValue = 0;
+  for (int i = 0; i <= (avgCount-1); i++) 
+  {
+      avgValue += getMeasurement();
+
+      yield();    
+      ESP.wdtFeed(); 
   }
-  server.send(404, "text/plain", message);
+  Serial.println("done");
+  Serial.println(avgCount);
+  Serial.println(avgValue);
+  avgValue = avgValue / avgCount;
+  Serial.println(avgValue);
+
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH  
+}
+
+void handleAvgMeasurement() {
+  Serial.println("handleAvgMeasurement()");
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+
+  String message = String(avgValue);
+
+  server.send(200, "text/plain", message);
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
 }
 
-void setup(void) {
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+void handleAvgPower() {
+  Serial.println("handleAvgPower");
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+
+  int value = avgValue;
+  Serial.println("Value:");
+  Serial.println(value);
+  float power = float(CALCURRENTPOWER)/float(CALCURRENTVALUE) * value;
+  Serial.println("Power");
+  Serial.println(power);
+
+  String message = String(power);
+  server.send(200, "text/plain", message);
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+}
 
-  // Wifi
-  Serial.begin(115200);
-    
+// --------------------------------------------
+
+void startWifi(void) {
+  Serial.println("StartWifi");
+
   WiFi.mode(WIFI_STA);
-
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
     Serial.println("STA Failed to configure");
   }
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
 
+  // Connect
   WiFi.begin(ssid, password);
-  Serial.println("Connecting");
+  Serial.println("Connecting Wifi");
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -156,6 +307,28 @@ void setup(void) {
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+void stopWifi(void) {
+  Serial.println("stopWifi");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+}
+
+void setup(void) {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+
+  Serial.begin(115200);
+  Serial.println("Serial Monitor");
+
+  // Watchdog
+  // Es gibt einen hardware und einen software watchdog
+  ESP.wdtDisable();     // Softwarewatchdog aus  
+  ESP.wdtEnable(5000);  // Softwarewatchdog auf n sek stellen, achtung wenn ich ihn abgeschaltet lasse löst aller 6-7 Sekunden der Hardware watchdog aus
+
+  // Wifi
+  startWifi();
 
   // MDNS
   if (MDNS.begin("esp8266")) {
@@ -164,18 +337,52 @@ void setup(void) {
 
   // Server
   server.on("/", handleRoot);
-  server.on("/sampleValues", handleSampleValues);
-  server.on("/maxValue", handleMaxValue);
 
-  server.on("/measurePower", handleMeasurePower);
+  server.on("/currentSampleValues", handleCurrentSampleValues);
+  server.on("/currentMeasurement", handleCurrentMeasurement);
+  server.on("/currentPower", handleCurrentPower);
 
-  server.onNotFound(handleNotFound);
+  // Mittelwertmessung ausführen
+  server.on("/avgDo", handleAvgDo);
+  // Mittelwertmessung abfragen
+  server.on("/avgFetchMeasurement", handleAvgMeasurement);
+  server.on("/avgFetchPower", handleAvgPower);
+ 
+  /*
+  server.on("/test", []() {
+    server.send(200, "text/plain", );
+  });
+  */
 
   server.begin();
   Serial.println("HTTP server started");
 }
 
+// --------------------------------------------
+
 void loop(void) {
-  server.handleClient();
+  // Main Loop
+  // der server.handleClient() muss permanent aufgerufen werden
+  // Blockieren länger als 20ms wird nicht empfohlen
+
   MDNS.update();
+  server.handleClient();
+
+  if (avgActive == true ) {
+    avgActive = false;
+    Serial.println("enter main loop hook");
+ 
+    //delay(1000); // yield() im Hintergrund, abarbeitung 
+    //stopWifi();
+
+    // führt zur Wifi Unterbrechung
+    // wenn reconect gesetzt verbindet er automatisch 
+    // ansonsten müsste hier das wifi manuell gestoppt und neu gestartet werden
+    doAvgMeasurement();
+
+    //startWifi();
+
+    Serial.println("leave main loop hook");
+  }
+
 }
